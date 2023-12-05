0be179db3ce525ae56af1f9b9c5dbe38647d2691
aaryanmehta26 <aaryanmehta2699@gmail.com>
2023-12-05 22:44:52 +0530

    Added socket connection on backend

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+const URL = 'http://localhost:3000';
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: 'http://localhost:3000',
-});
+const io = new Server(httpServer, { cors: URL });
 
 io.on('connection', (socket) => {
-  console.log('connection');
+  console.log('server connected');
+
+  socket.on('beginPath', (arg) => {
+    socket.broadcast.emit('beginPath', arg);
+  });
+
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg);
+  });
+
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg);
+  });
 });
 
 httpServer.listen(5000);
